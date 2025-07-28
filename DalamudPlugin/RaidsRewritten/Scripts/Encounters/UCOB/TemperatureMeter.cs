c38a23ec3fb6115e6daa6a3e062de30e60d12507
Adam <poison7senpai@gmail.com>
2025-07-28 18:59:57 -0400

    Updated delays, code cleanup, added delay functionality, updated variable names, added Afah multiplier
    Updated Delays to actual values
    Cleaned up unneeded libraries
    Updated logic to allow for delays
    Updated Reset call to properly cleanup attack array
    Added Morn Afah multiplier to increase heat for each morn afah added to hits

--- DalamudPlugin/RaidsRewritten/Scripts/Encounters/UCOB/TemperatureMeter.cs
+++ DalamudPlugin/RaidsRewritten/Scripts/Encounters/UCOB/TemperatureMeter.cs
@@ -1,14 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Numerics;
 using ECommons.Hooks;
 using ECommons.Hooks.ActionEffectTypes;
 using Flecs.NET.Core;
 using RaidsRewritten.Extensions;
 using RaidsRewritten.Game;
-using RaidsRewritten.Log;
 using RaidsRewritten.Scripts.Conditions;
-using RaidsRewritten.Utility;
 
 namespace RaidsRewritten.Scripts.Encounters.UCOB;
 
@@ -24,49 +21,66 @@ internal class TemperatureMeter : Mechanic
     private readonly Dictionary<uint, HeatData> HeatDict = new Dictionary<uint, HeatData>
     {
         {
-            9900, new HeatData { HeatValue = 50.0f, ID = 9900, DelaySeconds = 0.8f } //Fireball (Twin)
+            9900, new HeatData { HeatValue = 50.0f, ID = 9900, DelaySeconds = 0.6f } //Fireball (Twin)
         },
         {
-            9901, new HeatData { HeatValue = 20.0f, ID = 9901, DelaySeconds = 0.8f } //Liquid hell
+            9901, new HeatData { HeatValue = 20.0f, ID = 9901, DelaySeconds = 1.1f } //Liquid hell
         },
         {
             9917, new HeatData { HeatValue = 50.0f, ID = 9917, DelaySeconds = 0.8f } //Thermeonic Beam
         },
         {
-            9925, new HeatData { HeatValue = 50.0f, ID = 9925, DelaySeconds = 0.8f } //Fireball (Firehorn)
+            9925, new HeatData { HeatValue = 50.0f, ID = 9925, DelaySeconds = 1.0f } //Fireball (Firehorn)
         },
         {
-            9926, new HeatData { HeatValue = -50.0f, ID = 9926, DelaySeconds = 0.8f } //Iceball (Iceclaw)
+            9926, new HeatData { HeatValue = -50.0f, ID = 9926, DelaySeconds = 1.0f } //Iceball (Iceclaw)
         },
         {
-            9937, new HeatData { HeatValue = 100.0f, ID = 9937, DelaySeconds = 0.8f } //Seventh Umbral Era
+            9937, new HeatData { HeatValue = 100.0f, ID = 9937, DelaySeconds = 0.1f } //Seventh Umbral Era
         },
         {
-            9938, new HeatData { HeatValue = 20.0f, ID = 9938, DelaySeconds = 0.8f } //Calamitous Flame
+            9938, new HeatData { HeatValue = 20.0f, ID = 9938, DelaySeconds = 0.2f } //Calamitous Flame
         },
         {
-            9939, new HeatData { HeatValue = 20.0f, ID = 9939, DelaySeconds = 0.8f } //Calamitous Flame (Final hit)
+            9939, new HeatData { HeatValue = 20.0f, ID = 9939, DelaySeconds = 0.2f } //Calamitous Blaze (Final hit)
         },
         { 
-            9940, new HeatData { HeatValue = 20.0f, ID = 9940, DelaySeconds = 0.8f } //Flare Breath
+            9940, new HeatData { HeatValue = 20.0f, ID = 9940, DelaySeconds = 0.9f } //Flare Breath
         },
         { 
-            9942, new HeatData { HeatValue = 50.0f, ID = 9942, DelaySeconds = 0.8f} //Gigaflare
+            9942, new HeatData { HeatValue = 50.0f, ID = 9942, DelaySeconds = 3.6f} //Gigaflare
         },
         { 
-            9962, new HeatData { HeatValue = 20.0f, ID = 9962, DelaySeconds = 0.8f } //Ahk Morn (1st hit)
+            9962, new HeatData { HeatValue = 20.0f, ID = 9962, DelaySeconds = 0.9f } //Ahk Morn (1st hit)
         },
         { 
-            9963, new HeatData { HeatValue = 20.0f, ID = 9963, DelaySeconds = 0.8f } //Ahk Morn (2nd+ Hit)
+            9963, new HeatData { HeatValue = 20.0f, ID = 9963, DelaySeconds = 0.9f } //Ahk Morn (2nd+ Hit)
         },
         {
-            9964, new HeatData { HeatValue = 20.0f, ID = 9964, DelaySeconds = 0.8f } //Morn Afah
+            9964, new HeatData { HeatValue = 20.0f, ID = 9964, DelaySeconds = 0.9f } //Morn Afah
         }
+        //9970 Flames of Rebirth (20s till targetable)
     };
+
+    private readonly List<Entity> attacks = [];
+    private int AfahMultiplier = 1;
 
     public override void Reset()
     {
+        foreach (var attack in this.attacks)
+        {
+            attack.Destruct();
+        }
+        this.attacks.Clear();
+    }
 
+    public override void OnDirectorUpdate(DirectorUpdateCategory a3)
+    {
+        if (a3 == DirectorUpdateCategory.Wipe ||
+            a3 == DirectorUpdateCategory.Recommence)
+        {
+            Reset();
+        }
     }
 
     public override void OnActionEffectEvent(ActionEffectSet set)
@@ -75,22 +89,33 @@ internal class TemperatureMeter : Mechanic
         {
             if (set.Action == null) { return; }
             if (set.Source == null) { return; }
-            if (!HeatDict.TryGetValue(set.Action.Value.RowId, out var HeatData)) { return; }
-
+            if (!HeatDict.TryGetValue(set.Action.Value.RowId, out var Heat)) { return; }
+            
             var localPlayer = Dalamud.ClientState.LocalPlayer;
             if (localPlayer == null) { return; }
-            foreach (var targetEffects in set.TargetEffects)
+
+            var da = DelayedAction.Create(this.World, () =>     
             {
-                if (targetEffects.TargetID == localPlayer.GameObjectId)
+                foreach (var targetEffects in set.TargetEffects)
                 {
-                    using var q = World.Query<Player.Component>();
-                    q.Each((Entity e, ref Player.Component pc) =>
+                    if (targetEffects.TargetID == localPlayer.GameObjectId)
                     {
-                        Temperature.HeatChangedEvent(e, HeatData.HeatValue, 0, HeatData.ID);
-                    });
-                    return;
+                        using var q = World.Query<Player.Component>();
+                        q.Each((Entity e, ref Player.Component pc) =>
+                        {
+                            float HeatDelta = Heat.HeatValue;
+                            if (Heat.ID == 9964)
+                            {
+                                HeatDelta *= AfahMultiplier++;
+
+                            }
+                            Temperature.HeatChangedEvent(e, HeatDelta, 0, Heat.ID);
+                        });
+                        return;
+                    }
                 }
-            }
+            }, Heat.DelaySeconds);
+            this.attacks.Add(da);
         }
         catch (Exception e)
         {
