8227bf16a4a9d6c23447c6588a511eb05199645c
OTCompa <otcompa@gmail.com>
2026-02-06 22:31:11 -0600

    requested changes

--- DalamudPlugin/RaidsRewritten/UI/View/MainWindow.cs
+++ DalamudPlugin/RaidsRewritten/UI/View/MainWindow.cs
@@ -219,27 +219,9 @@ public sealed partial class MainWindow : Window, IPluginUIView, IDisposable
 
             if (ImGui.IsItemHovered())
             {
-                ImGui.SetTooltip("Native custom status rendering is incompatible with Moodles. Click here for more info");
+                ImGui.SetTooltip("RaidsRewritten is incompatible with Moodles.\r\nClick for more information.");
             }
-
-            var center = ImGui.GetMainViewport().GetCenter();
-            ImGui.SetNextWindowPos(center, ImGuiCond.Appearing, new Vector2(0.5f, 0.5f));
-            ImGui.SetNextWindowSize(new Vector2(500, 205));
-            using (var popup = ImRaii.PopupModal(incompatibilityId))
-            {
-                if (popup.Success)
-                {
-                    ImGui.TextWrapped("Native custom status rendering is incompatible with Moodles as this also uses the same system. " +
-                        "There is currently no functionality implemented in each plugin that makes it possible for both native custom status rendering to work in tandem. " +
-                        "While Moodles is enabled, RaidsRewritten will automatically use legacy custom status rendering so it's still possible raid with Moodles on. " +
-                        "However, we do think that your experience will be greatly improved with native custom status rendering.");
-                    ImGui.TextWrapped("Please temporarily disable Moodles while you use this plugin for the best experience!");
-                    if (ImGui.Button("Close"))
-                    {
-                        ImGui.CloseCurrentPopup();
-                    }
-                }
-            }
+            DrawMoodlesIncompatibilityModal();
         }
 
         using (var iconFont = ImRaii.PushFont(UiBuilder.IconFont))
@@ -266,6 +248,126 @@ public sealed partial class MainWindow : Window, IPluginUIView, IDisposable
         using var mainTab = ImRaii.TabItem("Main");
         if (!mainTab) return;
 
+        var encounterText = new StringBuilder("Active Encounter: ");
+        if (encounterManager.ActiveEncounter == null)
+        {
+            encounterText.Append("None");
+            ImGui.Text(encounterText.ToString());
+        }
+        else
+        {
+            encounterText.Append(encounterManager.ActiveEncounter.Name);
+            ImGui.Text(encounterText.ToString());
+
+            using (ImRaii.PushIndent())
+#if !DEBUG
+            using (ImRaii.Disabled(encounterManager.InCombat))
+#endif
+            {
+                encounterManager.ActiveEncounter.DrawConfig();
+            }
+        }
+    }
+
+    private void DrawMiscTab()
+    {
+        using var miscTab = ImRaii.TabItem("Misc");
+        if (!miscTab) return;
+
+        var useLegacyStatusRendering = this.configuration.UseLegacyStatusRendering;
+        {
+            using var disabled = ImRaii.Disabled(moodlesIPC.MoodlesPresent);
+            if (ImGui.Checkbox("Use legacy status rendering", ref useLegacyStatusRendering))
+            {
+                if (useLegacyStatusRendering)
+                {
+                    statusManager.HideAll();
+                }
+                this.configuration.UseLegacyStatusRendering = useLegacyStatusRendering;
+                this.configuration.Save();
+            }
+        }
+
+        using (var iconFont = ImRaii.PushFont(UiBuilder.IconFont))
+        {
+            var errorIcon = FontAwesomeIcon.ExclamationCircle.ToIconString();
+            ImGui.SameLine();
+            if (ImGui.Button(errorIcon))
+            {
+                ImGui.OpenPopup(incompatibilityId);
+            }
+        }
+        DrawMoodlesIncompatibilityModal();
+
+        if (useLegacyStatusRendering)
+        {
+            using (ImRaii.PushIndent())
+            {
+                DrawLegacyStatusOptions();
+            }
+        }
+
+        var printLogsToChat = this.configuration.PrintLogsToChat;
+        if (ImGui.Checkbox("Print logs to chat", ref printLogsToChat))
+        {
+            this.configuration.PrintLogsToChat = printLogsToChat;
+            this.configuration.Save();
+        }
+
+        if (printLogsToChat)
+        {
+            ImGui.SameLine();
+            var minLogLevel = this.configuration.MinimumVisibleLogLevel;
+            ImGui.SetNextItemWidth(70);
+            if (ImGui.Combo("Min log level", ref minLogLevel, allLoggingLevels, allLoggingLevels.Length))
+            {
+                this.configuration.MinimumVisibleLogLevel = minLogLevel;
+                this.configuration.Save();
+            }
+        }
+
+        if (ImGui.Button("Changelog"))
+        {
+            this.changelogWindow.Visible = true;
+        }
+
+        ImGui.Spacing();
+
+        ImGui.AlignTextToFramePadding();
+        ImGui.Text("Bugs or suggestions?\nWant to playtest the next Rewritten fight?");
+        ImGui.PushStyleColor(ImGuiCol.Button, new Vector4(0.35f, 0.40f, 0.95f, 1));
+        ImGui.PushStyleColor(ImGuiCol.ButtonHovered, new Vector4(0.41f, 0.45f, 1.0f, 1));
+        ImGui.PushStyleColor(ImGuiCol.ButtonActive, new Vector4(0.32f, 0.36f, 0.88f, 1));
+        if (ImGui.Button("Discord"))
+        {
+            Process.Start(new ProcessStartInfo { FileName = "https://discord.gg/rSucAJ6A7u", UseShellExecute = true });
+        }
+        ImGui.PopStyleColor(3);
+
+        //ImGui.SameLine();
+        //ImGui.Text("|");
+        //ImGui.SameLine();
+
+        //ImGui.PushStyleColor(ImGuiCol.Button, new Vector4(1.0f, 0.39f, 0.20f, 1));
+        //ImGui.PushStyleColor(ImGuiCol.ButtonHovered, new Vector4(1.0f, 0.49f, 0.30f, 1));
+        //ImGui.PushStyleColor(ImGuiCol.ButtonActive, new Vector4(0.92f, 0.36f, 0.18f, 1));
+        //if (ImGui.Button("Support on Ko-fi"))
+        //{
+        //    Process.Start(new ProcessStartInfo { FileName = "https://ko-fi.com/ricimon", UseShellExecute = true });
+        //}
+        //ImGui.PopStyleColor(3);
+    }
+
+    private void DisplayFakeStatus()
+    {
+        commonQueries.LocalPlayerQuery.Each((Entity e, ref Player.Component pc) =>
+        {
+            this.World.Entity().Set(new Condition.Component("Fake Status", 15.0f, DateTime.UtcNow)).ChildOf(e);
+        });
+    }
+
+    private void DrawLegacyStatusOptions()
+    {
         ImGui.PushItemWidth(120);
         var effectsRendererPositionX = configuration.EffectsRendererPositionX;
         if (ImGui.InputInt("Status Display Position X", ref effectsRendererPositionX, 5))
@@ -336,115 +438,63 @@ public sealed partial class MainWindow : Window, IPluginUIView, IDisposable
             configuration.EffectsRendererPositionY = y;
             configuration.Save();
         }
-
-        var encounterText = new StringBuilder("Active Encounter: ");
-        if (encounterManager.ActiveEncounter == null)
-        {
-            encounterText.Append("None");
-            ImGui.Text(encounterText.ToString());
-        }
-        else
-        {
-            encounterText.Append(encounterManager.ActiveEncounter.Name);
-            ImGui.Text(encounterText.ToString());
-
-            using (ImRaii.PushIndent())
-#if !DEBUG
-            using (ImRaii.Disabled(encounterManager.InCombat))
-#endif
-            {
-                encounterManager.ActiveEncounter.DrawConfig();
-            }
-        }
     }
 
-    private void DrawMiscTab()
+    private void DrawMoodlesIncompatibilityModal()
     {
-        using var miscTab = ImRaii.TabItem("Misc");
-        if (!miscTab) return;
-
-        var useLegacyStatusRendering = this.configuration.UseLegacyStatusRendering;
+        var center = ImGui.GetMainViewport().GetCenter();
+        ImGui.SetNextWindowPos(center, ImGuiCond.Appearing, new Vector2(0.5f, 0.5f));
+        ImGui.SetNextWindowSize(new Vector2(500, 205));
+        using (var popup = ImRaii.PopupModal(incompatibilityId))
         {
-            using var disabled = ImRaii.Disabled(moodlesIPC.MoodlesPresent);
-            if (ImGui.Checkbox("Use legacy status rendering", ref useLegacyStatusRendering))
+            if (popup.Success)
             {
-                if (useLegacyStatusRendering)
+                ImGui.TextWrapped("RaidsRewritten uses the same underlying system as Moodles to render native custom statuses. " +
+                    "However, this system does not work properly with another instance of the same system. It is recommended to disable Moodles while using RaidsRewritten.");
+                ImGui.TextWrapped("While this incompatibility is detected, RaidsRewritten will force legacy status rendering. " +
+                    "Please note that some mechanics may be much more difficult to resolve without native custom status rendering.");
+
+                if (ImGui.Button("Check Moodles status"))
                 {
-                    statusManager.HideAll();
+                    if (!moodlesIPC.CheckMoodles())
+                    {
+                        this.configuration.UseLegacyStatusRendering = false;
+                        this.configuration.Save();
+                        ImGui.CloseCurrentPopup();
+                    }
                 }
-                this.configuration.UseLegacyStatusRendering = useLegacyStatusRendering;
-                this.configuration.Save();
-            }
-        }
-        if (moodlesIPC.MoodlesPresent)
-        {
-            ImGui.TextWrapped("Native custom status rendering is incompatible with Moodles.\nPlease disable Moodles temporarily.");
-            if (ImGui.Button("Check Moodles status"))
-            {
-                if (!moodlesIPC.CheckMoodles())
+
+                ImGui.SameLine();
+                if (moodlesIPC.MoodlesPresent)
                 {
-                    this.configuration.UseLegacyStatusRendering = false;
-                    this.configuration.Save();
+                    using (ImRaii.PushColor(ImGuiCol.Text, Vector4Colors.Red))
+                    {
+                        using (ImRaii.PushFont(UiBuilder.IconFont))
+                        {
+                            ImGui.Text($"{FontAwesomeIcon.SquareXmark.ToIconChar()}");
+                        }
+
+                        ImGui.SameLine(0, 2f);
+                        ImGui.Text("Moodles is currently active.");
+                    }
+                } else
+                {
+                    using (ImRaii.PushColor(ImGuiCol.Text, Vector4Colors.Green))
+                    {
+                        using (ImRaii.PushFont(UiBuilder.IconFont))
+                        {
+                            ImGui.Text($"{FontAwesomeIcon.CheckSquare.ToIconChar()}");
+                            ImGui.SameLine(0, 2f);
+                        }
+                        ImGui.Text("Moodles not detected.");
+                    }
+                }
+
+                if (ImGui.Button("Close"))
+                {
+                    ImGui.CloseCurrentPopup();
                 }
             }
         }
-
-        var printLogsToChat = this.configuration.PrintLogsToChat;
-        if (ImGui.Checkbox("Print logs to chat", ref printLogsToChat))
-        {
-            this.configuration.PrintLogsToChat = printLogsToChat;
-            this.configuration.Save();
-        }
-
-        if (printLogsToChat)
-        {
-            ImGui.SameLine();
-            var minLogLevel = this.configuration.MinimumVisibleLogLevel;
-            ImGui.SetNextItemWidth(70);
-            if (ImGui.Combo("Min log level", ref minLogLevel, allLoggingLevels, allLoggingLevels.Length))
-            {
-                this.configuration.MinimumVisibleLogLevel = minLogLevel;
-                this.configuration.Save();
-            }
-        }
-
-        if (ImGui.Button("Changelog"))
-        {
-            this.changelogWindow.Visible = true;
-        }
-
-        ImGui.Spacing();
-
-        ImGui.AlignTextToFramePadding();
-        ImGui.Text("Bugs or suggestions?\nWant to playtest the next Rewritten fight?");
-        ImGui.PushStyleColor(ImGuiCol.Button, new Vector4(0.35f, 0.40f, 0.95f, 1));
-        ImGui.PushStyleColor(ImGuiCol.ButtonHovered, new Vector4(0.41f, 0.45f, 1.0f, 1));
-        ImGui.PushStyleColor(ImGuiCol.ButtonActive, new Vector4(0.32f, 0.36f, 0.88f, 1));
-        if (ImGui.Button("Discord"))
-        {
-            Process.Start(new ProcessStartInfo { FileName = "https://discord.gg/rSucAJ6A7u", UseShellExecute = true });
-        }
-        ImGui.PopStyleColor(3);
-
-        //ImGui.SameLine();
-        //ImGui.Text("|");
-        //ImGui.SameLine();
-
-        //ImGui.PushStyleColor(ImGuiCol.Button, new Vector4(1.0f, 0.39f, 0.20f, 1));
-        //ImGui.PushStyleColor(ImGuiCol.ButtonHovered, new Vector4(1.0f, 0.49f, 0.30f, 1));
-        //ImGui.PushStyleColor(ImGuiCol.ButtonActive, new Vector4(0.92f, 0.36f, 0.18f, 1));
-        //if (ImGui.Button("Support on Ko-fi"))
-        //{
-        //    Process.Start(new ProcessStartInfo { FileName = "https://ko-fi.com/ricimon", UseShellExecute = true });
-        //}
-        //ImGui.PopStyleColor(3);
-    }
-
-    private void DisplayFakeStatus()
-    {
-        commonQueries.LocalPlayerQuery.Each((Entity e, ref Player.Component pc) =>
-        {
-            this.World.Entity().Set(new Condition.Component("Fake Status", 15.0f, DateTime.UtcNow)).ChildOf(e);
-        });
     }
 }
