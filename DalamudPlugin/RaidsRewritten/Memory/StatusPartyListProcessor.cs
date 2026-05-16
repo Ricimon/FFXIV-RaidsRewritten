40c58c70818741c1fb2d2683d528bca775d308f3
Frey <41562938+OTCompa@users.noreply.github.com>
2026-05-16 02:22:52 -0500

    remove accidental imports

--- DalamudPlugin/RaidsRewritten/Memory/StatusPartyListProcessor.cs
+++ DalamudPlugin/RaidsRewritten/Memory/StatusPartyListProcessor.cs
@@ -24,8 +24,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using WinRT;
-using static FFXIVClientStructs.FFXIV.Client.UI.AddonJobHudMNK1.ChakraGauge;
 
 namespace RaidsRewritten.Memory;
 
