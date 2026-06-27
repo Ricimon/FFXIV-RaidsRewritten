7ba8053d76b55c1b32713d331667abe4b9418361
Ricimon <ricimon25@gmail.com>
2026-06-26 18:19:39 -0700

    Fire tornado WIP

---
+++ DalamudPlugin/RaidsRewritten/Scripts/Encounters/TEA/FireTornado.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Dalamud.Game.ClientState.Objects.Types;
+using ECommons.Hooks;
+using Flecs.NET.Core;
+using RaidsRewritten.Scripts.Components;
+
+namespace RaidsRewritten.Scripts.Encounters.TEA;
+
+public class FireTornado : Mechanic
+{
+    private const uint LIQUID_RAGE_DATA_ID = 0x2C49;
+
+    private readonly List<Entity> attacks = [];
+    private readonly IReadOnlyList<Vector3> tornadoPositions =
+        [new(85, 0, 100),
+        new(115, 0, 100),
+        new(100, 0, 85),
+        new(100, 0, 115)];
+
+    private HashSet<Vector3> availableTornadoPositions = [];
+
+    public override void Reset()
+    {
+        foreach (var attack in this.attacks)
+        {
+            attack.Destruct();
+        }
+        this.attacks.Clear();
+        availableTornadoPositions = [.. tornadoPositions];
+    }
+
+    public override void OnDirectorUpdate(DirectorUpdateCategory a3)
+    {
+        if (a3 == DirectorUpdateCategory.Wipe ||
+            a3 == DirectorUpdateCategory.Recommence)
+        {
+            Reset();
+        }
+    }
+
+    public override void OnCombatEnd()
+    {
+        Reset();
+    }
+
+    public override void OnObjectCreation(nint newObjectPointer, IGameObject? newObject)
+    {
+        if (newObject == null) { return; }
+        if (newObject.BaseId != LIQUID_RAGE_DATA_ID) { return; }
+
+        availableTornadoPositions.Remove(newObject.Position);
+
+        if (availableTornadoPositions.Count == 1)
+        {
+            var position = availableTornadoPositions.ElementAt(0);
+            availableTornadoPositions.Clear();
+
+            var tornado = World.Entity()
+                .Set(new Model(1666))
+                .Set(new Position(position))
+                .Set(new Rotation())
+                .Set(new Scale())
+                .Set(new UniformScale(2.5f))
+                .Add<Attack>();
+
+            attacks.Add(tornado);
+        }
+    }
+}
--- DalamudPlugin/RaidsRewritten/Scripts/Encounters/TEA/TeaRewritten.cs
+++ DalamudPlugin/RaidsRewritten/Scripts/Encounters/TEA/TeaRewritten.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using Dalamud.Bindings.ImGui;
 
 namespace RaidsRewritten.Scripts.Encounters.TEA;
 
@@ -8,7 +9,33 @@ public class TeaRewritten : IEncounter
 
     public string Name => "TEA Rewritten";
 
+    // Config
+    private string RngSeedKey => $"{Name}.RngSeed";
+    private string FireTornadoKey => $"{Name}.FireTornado";
+
+    private readonly Mechanic.Factory mechanicFactory;
+    private readonly DalamudServices dalamud;
+    private readonly Configuration configuration;
+
     private readonly List<Mechanic> mechanics = [];
+    private readonly Dictionary<string, bool> defaultBoolSettings;
+    private readonly Dictionary<string, int> defaultIntSettings;
+
+    public TeaRewritten(Mechanic.Factory mechanicFactory, DalamudServices dalamud, Configuration configuration)
+    {
+        this.mechanicFactory = mechanicFactory;
+        this.dalamud = dalamud;
+        this.configuration = configuration;
+
+        this.defaultBoolSettings = new()
+        {
+            { FireTornadoKey, true },
+        };
+
+        this.defaultIntSettings = new()
+        {
+        };
+    }
 
     public IEnumerable<Mechanic> GetMechanics()
     {
@@ -18,6 +45,11 @@ public class TeaRewritten : IEncounter
     public void RefreshMechanics()
     {
         Unload();
+
+        if (configuration.GetEncounterSetting(FireTornadoKey, this.defaultBoolSettings[FireTornadoKey]))
+        {
+            this.mechanics.Add(mechanicFactory.Create<FireTornado>());
+        }
     }
 
     public void Unload()
@@ -35,5 +67,50 @@ public class TeaRewritten : IEncounter
 
     public void DrawConfig()
     {
+        if (ImGui.Button("Apply Intended Difficulty"))
+        {
+            ApplyIntendedFightSettings();
+        }
+        ImGui.SameLine();
+        if (ImGui.Button("Disable Everything"))
+        {
+            DisableEverything();
+        }
+
+        bool fireTornado = configuration.GetEncounterSetting(FireTornadoKey, this.defaultBoolSettings[FireTornadoKey]);
+        if (ImGui.Checkbox("Fire Tornado", ref fireTornado))
+        {
+            configuration.EncounterSettings[FireTornadoKey] =
+                fireTornado ? bool.TrueString : bool.FalseString;
+            configuration.Save();
+            RefreshMechanics();
+        }
+    }
+
+    private void ApplyIntendedFightSettings()
+    {
+        foreach(var setting in this.defaultBoolSettings)
+        {
+            configuration.EncounterSettings[setting.Key] = setting.Value.ToString();
+        }
+
+        foreach(var setting in this.defaultIntSettings)
+        {
+            configuration.EncounterSettings[setting.Key] = setting.Value.ToString();
+        }
+
+        configuration.Save();
+        RefreshMechanics();
+    }
+
+    private void DisableEverything()
+    {
+        foreach(var setting in this.defaultBoolSettings)
+        {
+            configuration.EncounterSettings[setting.Key] = bool.FalseString;
+        }
+
+        configuration.Save();
+        RefreshMechanics();
     }
 }
