414f52e841adee2fb1ab708e438eebecbc545064
Ricimon <ricimon25@gmail.com>
2026-09-05 19:40:03 -0700

    patch shanoa nisi interaction

--- raidsrewritten_server/src/game/mechanics/1020-tea_spawn_shanoa.rs
+++ raidsrewritten_server/src/game/mechanics/1020-tea_spawn_shanoa.rs
@@ -15,6 +15,7 @@ pub struct TeaShanoa {
     pub fire_tornado: Entity,
     pub movement_speed: f32,
     pub rotation_speed: f32,
+    pub nisi_interaction_timer: f32,
 }
 
 #[derive(Component, Debug)]
@@ -85,6 +86,7 @@ pub fn create_systems(world: &World) {
                         fire_tornado: *entity,
                         movement_speed: 6.0,
                         rotation_speed: 7.0,
+                        nisi_interaction_timer: 0.0,
                     })
                     .set(Position {
                         x: shanoa_position.x,
